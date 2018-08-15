261cb5195f989e42211da9348d5452aa437b5b03
romik <makavanaromik1214@gmail.com>
2018-08-15 10:55:49 +0530

    try now

--- pages/site/index.php
+++ pages/site/index.php
@@ -1,3 +1,4 @@
 <div align="center" style="height: 100vh; padding-top: 25vh;">
-		<h1 class="romik">MVC Frame  <sub>by Romik</sub></h1>
-	</div>
+		<h1 class="romik">Romik<sub> 0.1</sub></h1>
+		<h3>PHP MVC Framework</h3>
+</div>
